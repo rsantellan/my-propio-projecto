da0d6f87b0ee8f0449edcbda3e2e54abc97e9106
rsantellan@gmail.com <rsantellan@gmail.com@813e5b4f-ef03-7a5b-8fff-a97fd87d9e6f>
2012-07-22 22:34:10 +0000

    Se agrega el form para publicar .

---
+++ lib/form/apartamentaPublicar.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of apartamentaPublicar
+ *
+ * @author Rodrigo Santellan
+ */
+class apartamentaPublicar extends mdApartamentoForm {
+  //put your code here
+}
+
+
