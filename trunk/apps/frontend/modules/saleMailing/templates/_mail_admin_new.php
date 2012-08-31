84d0859b8aadcc88e01a2bdf97174bf07dd84ea7
rsantellan@gmail.com <rsantellan@gmail.com@813e5b4f-ef03-7a5b-8fff-a97fd87d9e6f>
2012-08-31 15:32:39 +0000

    Se corrigen los templates de mails

--- trunk/apps/frontend/modules/saleMailing/templates/_mail_admin_new.php
+++ trunk/apps/frontend/modules/saleMailing/templates/_mail_admin_new.php
@@ -5,74 +5,123 @@ $reserva = $item->getObject();
 $depto = $reserva->getmdApartamento();
 use_helper('Text');
 ?>
-<html>
-<body style="background-color:#F5F5F5">
-    <title><?php echo __('Mail_Admin Final Titulo', array('[tipo]'=>$depto->getTipo())); ?></title>
-    <table width="732" cellpadding="0" cellspacing="0" align="center">
-    	<tr>
-          <td><?php echo image_tag('/images/header-email.png', array('absolute' => true)); ?></td>
-        </tr>
-        <tr style="background-color:#FFFFFF">
-          <td><b style="font-family:Arial; color:#00B800; font-size:18px; padding-left:24px; padding-top:20px; padding-bottom:10px">
-					<?php echo __('Mail_Admin Final Titulo', array('[tipo]'=>$depto->getTipo())); ?></b>
-        </tr>
-        <tr style="background-color:#FFFFFF"><td height="10"></td></tr>
-        <tr style="background-color:#FFFFFF">
-          <td style="color:#414141; padding-left:24px; font-family:arial; font-size:12px">
-            <?php echo simple_format_text(__('Mail_Admin Final Cuerpo')); ?>
-          </td>
-        </tr>
-        <tr style="background-color:#FFFFFF">
-          <td style="color:#414141; padding-left:24px; font-family:arial; font-size:12px; padding-top:20px">
-    				<table width="80%" cellpadding="10" cellspacing="0" align="center">
-							<tr>
-								<td with="100px"><b style="font-family:Arial; color:#00B800; font-size:13px; margin-top:10px; margin-bottom:5px">
-									<?php echo __('Mail_Admin Final Locacion'); ?></b>
-				        </td>
-								<td>
-									<?php echo $depto->getmdLocacion()->getNombre() ?>
-								</td>
-							</tr>
-							<tr>
-								<td with="100px"><b style="font-family:Arial; color:#00B800; font-size:13px; margin-top:10px; margin-bottom:5px">
-									<?php echo __('Mail_Admin Final Apartamento'); ?></b>
-				        </td>
-								<td>
-									<a href="<?php echo url_for('apartamento',$depto, array('absolute'=>true)) ?>"><?php echo $depto->getTitulo() ?></a>
-								</td>
-							</tr>
-							<tr>
-								<td with="100px"><b style="font-family:Arial; color:#00B800; font-size:13px; margin-top:10px; margin-bottom:5px">
-									<?php echo __('Mail_Admin Final Fecha'); ?></b>
-				        </td>
-								<td>
-									<?php echo $reserva->getDateTimeObject('fecha_desde')->format('Y/m/d') ?> - 
-									<?php echo $reserva->getDateTimeObject('fecha_hasta')->format('Y/m/d') ?></a>
-								</td>
-							</tr>
-							<tr>
-								<td with="100px"><b style="font-family:Arial; color:#00B800; font-size:13px; margin-top:10px; margin-bottom:5px">
-									<?php echo __('Mail_Admin Final Usuario'); ?></b>
-				        </td>
-								<td>
-									<a href="<?php echo url_for('profile',$depto->getMdUser(),array('absolute'=>true)) ?>">
-									<?php echo $depto->getMdUser()->getMdUserProfile()->getName() . ' ' .$depto->getMdUser()->getMdUserProfile()->getLastName()?></a>
-								</td>
-							</tr>
-							<tr>
-								<td with="100px"><b style="font-family:Arial; color:#00B800; font-size:13px; margin-top:10px; margin-bottom:5px">
-									<?php echo __('Mail_Admin Final Email'); ?></b>
-				        </td>
-								<td>
-									<a href="mailto:<?php echo $depto->getMdUser()->getEmail()?>">
-									<?php echo $depto->getMdUser()->getEmail()?></a>
-								</td>
-							</tr>
-						</table>
-          </td>
-        </tr>
-        <tr style="background-color:#FFFFFF"><td height="20"></td></tr>
-        <tr><td height="2" bgcolor="#72D400"></td></tr>
-    </table>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?php echo __('Mail_Admin Final Titulo', array('[tipo]'=>$depto->getTipo())); ?></title>
+<style type="text/css">
+body,td,th {
+	font-family: "Courier New", Courier, monospace;
+	font-size: 12px;
+	color: #000;
+}
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+a:link {
+	color: #3DA333;
+	text-decoration: none;
+}
+a:visited {
+	color: #3DA333;
+	text-decoration: none;
+}
+a:hover {
+	color: #7FB61A;
+	text-decoration: none;
+}
+a:active {
+	color: #3DA333;
+	text-decoration: none;
+}
+</style>
+</head>
+
+<body>
+<table width="300" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td colspan="3">
+      <?php echo image_tag('/images/cabezal_mail.jpg', array('absolute' => true, 'width' => 600, 'height' => 140)); ?>
+    </td>
+  </tr>
+  <tr>
+    <td width="20" rowspan="3">&nbsp;</td>
+    <td width="560">&nbsp;</td>
+    <td width="20" rowspan="3">&nbsp;</td>
+  </tr>
+  <tr>
+    <td>
+      <?php echo simple_format_text(__('Mail_Admin Final Cuerpo')); ?>      
+    </td>
+  </tr>
+  <tr>
+    <td>
+        <?php echo $depto->getmdLocacion()->getNombre() ?>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <b>
+        <?php echo __('Mail_Admin Final Apartamento'); ?>
+      </b>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <a href="<?php echo url_for('apartamento',$depto, array('absolute'=>true)) ?>"><?php echo $depto->getTitulo() ?></a>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <b>
+        <?php echo __('Mail_Admin Final Fecha'); ?>
+      </b>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <?php echo $reserva->getDateTimeObject('fecha_desde')->format('Y/m/d') ?> - 
+      <?php echo $reserva->getDateTimeObject('fecha_hasta')->format('Y/m/d') ?></a>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <b>
+        <?php echo __('Mail_Admin Final Usuario'); ?>
+      </b>
+    </td>
+  </tr>
+  <tr>  
+    <td>
+      <a href="<?php echo url_for('profile',$depto->getMdUser(),array('absolute'=>true)) ?>">
+        <?php echo $depto->getMdUser()->getMdUserProfile()->getName() . ' ' .$depto->getMdUser()->getMdUserProfile()->getLastName()?>
+      </a>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <b>
+        <?php echo __('Mail_Admin Final Email'); ?>
+      </b>
+    </td>
+  </tr>
+  <tr>  
+    <td>
+      <a href="mailto:<?php echo $depto->getMdUser()->getEmail()?>">
+        <?php echo $depto->getMdUser()->getEmail()?></a>
+    </td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="5" colspan="3" bgcolor="#000000"></td>
+  </tr>
+</table>
 </body>
 </html>
