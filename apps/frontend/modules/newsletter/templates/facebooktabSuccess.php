c0ecd0d4d921e9880a2f2a4ecee3581426dc8343
rsantellan <rsantellan@813e5b4f-ef03-7a5b-8fff-a97fd87d9e6f>
2012-10-19 14:23:08 +0000

    Se agrega el tab de facebook.

---
+++ apps/frontend/modules/newsletter/templates/facebooktabSuccess.php
@@ -0,0 +1,23 @@
+<?php
+use_stylesheet('facebooktab.css');
+
+?>
+<form name="<?php echo $form->getName();?>" class="form-send" method="POST" action="<?php echo url_for('@newsletter') ?>">
+      <?php echo $form->renderHiddenFields() ?>
+    <div class="campos-left">
+        <li><?php echo __('novedades_nombre') ?></li>
+        <li><?php echo $form['nombre']->render(array('tabIndex'=>1)) ?></li>
+        <li><?php echo __('novedades_email') ?></li>
+        <li><?php echo $form['email']->render(array('tabIndex'=>2)) ?></li>
+
+        <div class="div-send"><button class="send" type="submit" tabIndex="4"><?php echo __('novedades_Enviar') ?></button></div>
+    </div>
+
+                  <div class="clear"></div>
+                  <ul class="error_list">
+                      <?php foreach($form->getFormattedErrors() as $error): ?>
+                      <li><?php echo $error; ?></li>
+                      <?php endforeach; ?>
+                  </ul>
+                  <div class="clear"></div>
+</form>
