a7c9354caf1417c9961737b33725d738b789e54b
rsantellan <rsantellan@813e5b4f-ef03-7a5b-8fff-a97fd87d9e6f>
2012-10-24 18:19:10 +0000

    Se corrige el newsletter para envios con mas de 500 usuarios.

---
+++ trunk/apps/backend/modules/default/actions/indexAction.class.php
@@ -0,0 +1,12 @@
+<?php
+
+class indexAction extends sfAction {
+  public function execute($request)
+  {
+    
+//    mdNewsletterHandler::sendAllNotSendedMails();
+//    die('aca');
+    
+  }
+      
+}
