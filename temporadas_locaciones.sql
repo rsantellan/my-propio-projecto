8e9054dbfad1c1a04bc7aaf82f4ae3be6a7e4de3
rsantellan <rsantellan@813e5b4f-ef03-7a5b-8fff-a97fd87d9e6f>
2012-12-06 14:13:02 +0000

    Se agrega el sql.

---
+++ temporadas_locaciones.sql
@@ -0,0 +1,33 @@
+INSERT INTO `md_locacion_temporada` (`md_locacion_id`, `date_from`, `date_to`, `tipo`) VALUES
+(1, '2012-12-20', '2013-01-06', 'Reveillon'),
+(1, '2013-01-07', '2013-01-31', 'Enero'),
+(1, '2013-02-01', '2013-02-28', 'Febrero'),
+(1, '2013-03-01', '2013-06-21', 'Primavera / Otoño'),
+(1, '2013-06-22', '2013-09-21', 'Invierno'),
+(1, '2013-09-22', '2013-12-19', 'Primavera / Otoño');
+
+INSERT INTO `md_locacion_temporada` (`md_locacion_id`, `date_from`, `date_to`, `tipo`) VALUES
+(2, '2012-12-20', '2013-01-06', 'Reveillon'),
+(2, '2013-01-07', '2013-01-31', 'Enero'),
+(2, '2013-02-01', '2013-02-28', 'Febrero'),
+(2, '2013-03-01', '2013-06-21', 'Primavera / Otoño'),
+(2, '2013-06-22', '2013-09-21', 'Invierno'),
+(2, '2013-09-22', '2013-12-19', 'Primavera / Otoño');
+
+INSERT INTO `md_locacion_temporada` (`md_locacion_id`, `date_from`, `date_to`, `tipo`) VALUES 
+(3, '2012-12-20', '2013-01-06', 'Reveillon'),
+(3, '2013-01-07', '2013-01-31', 'Enero'),
+(3, '2013-02-01', '2013-02-28', 'Febrero'),
+(3, '2013-03-01', '2013-06-21', 'Primavera / Otoño'),
+(3, '2013-06-22', '2013-09-21', 'Invierno'),
+(3, '2013-09-22', '2013-12-19', 'Primavera / Otoño');
+
+INSERT INTO `md_locacion_temporada` (`md_locacion_id`, `date_from`, `date_to`, `tipo`) VALUES 
+(4, '2012-12-20', '2013-01-06', 'Reveillon'),
+(4, '2013-01-07', '2013-01-31', 'Enero'),
+(4, '2013-02-01', '2013-02-28', 'Febrero'),
+(4, '2013-03-01', '2013-06-21', 'Primavera / Otoño'),
+(4, '2013-06-22', '2013-09-21', 'Invierno'),
+(4, '2013-09-22', '2013-12-19', 'Primavera / Otoño');
+
+
